007d8db730678603dd3fdde9daafca4b0e4767cd
Soundwave <PS-Soundwave@users.noreply.github.com>
2019-06-23 12:43:38 -0700

    Add gene dump command

---
+++ src/main/java/org/terasology/projsndwv/commands/BeeCommands.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.projsndwv.commands;
+
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.characters.CharacterHeldItemComponent;
+import org.terasology.logic.console.commandSystem.annotations.Command;
+import org.terasology.logic.console.commandSystem.annotations.Sender;
+import org.terasology.logic.permission.PermissionManager;
+import org.terasology.network.ClientComponent;
+import org.terasology.projsndwv.genetics.components.GeneticsComponent;
+
+@RegisterSystem
+public class BeeCommands extends BaseComponentSystem {
+    @Command(shortDescription = "Shows the genes of a held bee",
+        helpText = "Displays the raw genetic data from a bee currently being held",
+        runOnServer = true,
+        requiredPermission = PermissionManager.CHEAT_PERMISSION)
+    public String dumpGenes(@Sender EntityRef client) {
+        GeneticsComponent genetics = client.getComponent(ClientComponent.class).character.getComponent(CharacterHeldItemComponent.class).selectedItem.getComponent(GeneticsComponent.class);
+        if (genetics != null) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Active: ");
+            for (int i = 0; i < genetics.activeGenes.size(); i++) {
+                sb.append(genetics.activeGenes.get(i));
+                if (i != genetics.activeGenes.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append("\nInactive: ");
+            for (int i = 0; i < genetics.inactiveGenes.size(); i++) {
+                sb.append(genetics.inactiveGenes.get(i));
+                if (i != genetics.inactiveGenes.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+            return sb.toString();
+        }
+        return "Held item contains no genetic information.";
+    }
+}
