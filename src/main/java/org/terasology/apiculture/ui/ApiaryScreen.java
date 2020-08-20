ecf4d67f92752a68a7d96337e1b206316c957a27
DarkWeird <foxflameless@gmail.com>
2020-08-20 17:17:56 +0300

    fix: NUI extraction compatibility

--- src/main/java/org/terasology/apiculture/ui/ApiaryScreen.java
+++ src/main/java/org/terasology/apiculture/ui/ApiaryScreen.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.apiculture.ui;
 
 import org.terasology.engine.Time;
@@ -25,8 +12,8 @@ import org.terasology.apiculture.systems.ApiarySystem;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.registry.In;
 import org.terasology.rendering.nui.BaseInteractionScreen;
-import org.terasology.rendering.nui.Color;
-import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
+import org.terasology.nui.Color;
+import org.terasology.nui.databinding.ReadOnlyBinding;
 import org.terasology.rendering.nui.layers.ingame.inventory.InventoryGrid;
 
 import java.util.Objects;
