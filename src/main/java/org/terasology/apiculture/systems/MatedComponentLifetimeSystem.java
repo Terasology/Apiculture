c438de082de1a5bc27f88862881ad1b6348ab724
Nail Khanipov <foxflameless@gmail.com>
2021-12-05 22:10:24 +0300

    feat(gestaltv7-eventsystem): Migration Event and @ReceiveEvent to gestalt's (#19)

--- src/main/java/org/terasology/apiculture/systems/MatedComponentLifetimeSystem.java
+++ src/main/java/org/terasology/apiculture/systems/MatedComponentLifetimeSystem.java
@@ -18,10 +18,10 @@ package org.terasology.apiculture.systems;
 import org.terasology.apiculture.components.MatedComponent;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.entity.lifecycleEvents.BeforeRemoveComponent;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
 
 @RegisterSystem(RegisterMode.ALWAYS) // TODO: Try to make AUTHORITY
 public class MatedComponentLifetimeSystem extends BaseComponentSystem {
